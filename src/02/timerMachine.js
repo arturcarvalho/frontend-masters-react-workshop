60bca45a71cc579a413f864440a70794cf6a116d
Artur Carvalho <arturbelico@gmail.com>
2022-01-10 13:54:57 +0100

    Parameterize actions

--- src/02/timerMachine.js
+++ src/02/timerMachine.js
@@ -1,39 +1,46 @@
 import { createMachine, assign } from "xstate";
 
-export const timerMachine = createMachine({
-  initial: "idle",
-  context: {
-    duration: 60, // secs
-    elapsed: 0, // secs
-    interval: 0.1, // 1/10th of a sec
-  },
-  states: {
-    idle: {
-      // Reset duration and elapsed on entry
-      actions: assign({
-        duration: 60,
-        elapsed: 0,
-      }),
-
-      on: {
-        TOGGLE: "running",
-      },
+export const timerMachine = createMachine(
+  {
+    initial: "idle",
+    context: {
+      duration: 60, // secs
+      elapsed: 0, // secs
+      interval: 0.1, // 1/10th of a sec
     },
-    running: {
-      on: {
-        TOGGLE: "paused",
-        ADD_MINUTE: {
-          actions: assign({
-            duration: (ctx) => ctx.duration + 60,
-          }),
+    states: {
+      idle: {
+        // Reset duration and elapsed on entry
+        entry: assign({
+          duration: 60,
+          elapsed: 0,
+        }),
+
+        on: {
+          TOGGLE: "running",
+        },
+      },
+      running: {
+        on: {
+          TOGGLE: "paused",
+          ADD_MINUTE: {
+            actions: "addMinute",
+          },
+        },
+      },
+      paused: {
+        on: {
+          TOGGLE: "running",
+          RESET: "idle",
         },
       },
     },
-    paused: {
-      on: {
-        TOGGLE: "running",
-        RESET: "idle",
-      },
-    },
   },
-});
+  {
+    actions: {
+      addMinute: assign({
+        duration: (ctx) => ctx.duration + 60,
+      }),
+    },
+  }
+);
